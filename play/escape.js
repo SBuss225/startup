d7f82483e0d7256a4bfee07c26025f632c3b1781
Sarah Cornaby <sarah.cornaby@gmail.com>
2023-03-10 23:41:22 -0700

    escape mechanic implemented

---
+++ play/escape.js
@@ -0,0 +1,30 @@
+const CODE = "123";
+var userInput = ['*', '*', '*'];
+
+function attemptEscape() {
+  if (CODE == userInput.join("")) {
+    document.getElementById("locked").style.display = "none";
+    document.getElementById("unlocked").style.display = "block";
+    window.location.href = "../leaderboard/leaderboard.html";
+  }
+  userInput = ['*', '*', '*'];
+  document.getElementById("num-screen").innerText = userInput.join("");
+}
+
+function pressKey(key) {
+  document.getElementById("locked").style.display = "block";
+  document.getElementById("unlocked").style.display = "none";
+  
+  if (userInput.find(char => char === '*') == undefined) {
+    return;
+  }
+  
+  for (let i = 0; i < userInput.length; i++) {
+    if (userInput[i] == '*') {
+      userInput[i] = key;
+      break;
+    }
+  }
+  
+  document.getElementById("num-screen").innerText = userInput.join("");
+}
