baecc9073b94b6b1bb2db0f76358aadbe9406733
Sarah Cornaby <sarah.cornaby@gmail.com>
2023-03-10 22:10:40 -0700

    added basic navigation and login

---
+++ login.js
@@ -0,0 +1,21 @@
+function signup() {
+    const userNameEl = document.getElementById("s-username");
+    const emailEl = document.getElementById("s-email");
+    const passwordEl = document.getElementById("s-password");
+
+    localStorage.setItem("username", userNameEl.value);
+    localStorage.setItem("email", emailEl.value);
+    localStorage.setItem("password", passwordEl.value);
+
+    window.location.href = "play/play.html";
+}
+
+function login() {
+    const userNameEl = document.getElementById("l-username");
+    const passwordEl = document.getElementById("l-password");
+
+    localStorage.setItem("username", userNameEl.value);
+    localStorage.setItem("password", passwordEl.value);
+
+    window.location.href = "play/play.html";
+}
